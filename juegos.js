b9a4050619eb24af49d08f524bc7ee9bdb11c375
JuanG145 <153041291+JuanG145@users.noreply.github.com>
2026-08-14 13:47:38 -0500

    Add files via upload

---
+++ juegos.js
@@ -0,0 +1,19 @@
+const datosMenu = {
+    "fondoMenu": "",
+    "musicaMenu": "",
+    "loopMusica": true,
+    "animacionMenu": "libre", // "libre", "fila", "carrusel", "cuadricula"
+    "juegos": [
+        {
+            "id": "superfighters",
+            "nombre": "Superfighters",
+            "anio": "2026",
+            "jugadores": "1 Player",
+            "descripcion": "Nuevo juego agregado.",
+            "archivo": "Juegos/Super_Fighters_Ultimate.swf",
+            "portada": "SuperFighter.png",
+            "colorBorde": "#ffcc00",
+            "posicion": { "x": "13.0%", "y": "30.0%" }
+        }
+    ]
+};
